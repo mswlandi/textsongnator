08994951d29d8729f49b3490a468960f1a75c286
Henry <38134284+akahenry@users.noreply.github.com>
2019-10-28 02:58:56 -0300

    UPD - Adding comments

--- userinterface.py
+++ userinterface.py
@@ -33,27 +33,39 @@ class Root(FloatLayout):
     text_input = ObjectProperty(None)
     play = Player()
 
+    ## dismiss_popup: void -> void
+    ## Objective: remove pop up from screen.
     def dismiss_popup(self):
         self._popup.dismiss()
 
+    ## show_load: void -> void
+    ## Objective: creates the popup with file chooser where the user can choose a file
+    ##  to be loaded at text editor.
     def show_load(self):
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
+    ## show_save: void -> void
+    ## Objective: creates the popup with file chooser where the user can choose where
+    ##  the MIDI generated file will be saved.
     def show_save(self):
         content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
         self._popup = Popup(title="Save file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
+    ## load: str str -> void
+    ## Objective: put the text loaded from file to text_input attribute.
     def load(self, path, filename):
         with open(os.path.join(path, filename[0])) as stream:
             self.text_input.text = stream.read()
 
         self.dismiss_popup()
 
+    ## save: str str -> void
+    ## Objective: saves the MIDI file into user disk.
     def save(self, path, filename):
         if filename[-4:] != ".mid":
             filename = filename + ".mid"
@@ -61,6 +73,8 @@ class Root(FloatLayout):
 
         self.dismiss_popup()
 
+    ## play_song: void -> void
+    ## Objective: it creates the song and then plays it.
     def play_song(self):
         self.play.clear()
         self.play.playSong(self.text_input.text)
@@ -72,7 +86,7 @@ class Root(FloatLayout):
 class Textsongnator(App):
     pass
 
-
+# Loading from .kv file
 Factory.register('Root', cls=Root)
 Factory.register('LoadDialog', cls=LoadDialog)
 Factory.register('SaveDialog', cls=SaveDialog)
